d468ae819fe2b9f4ba6af2e748b861faaba05a65
wargio <wargio@libero.it>
2022-09-05 01:02:38 +0200

    Add free-pascal demangler (fpc 2.6.x to 3.2.x)

---
+++ src/pascal/pascal.c
@@ -0,0 +1,140 @@
+// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+#include "demangler_util.h"
+#include <rz_libdemangle.h>
+#include <ctype.h>
+
+#define IS_NAME(x) (IS_LOWER(x) || IS_DIGIT(x) || (x) == '_')
+
+/**
+ * \brief      Demangles freepascal 2.6.x to 3.2.x symbols
+ *
+ * \param      mangled      The mangled string
+ * \param[in]  mangled_len  The mangled string length
+ *
+ * \return     Demangled string on success otherwise NULL
+ */
+static char *demangle_free_pascal(char *mangled, size_t mangled_len) {
+	char *tmp = NULL;
+	char *next = mangled;
+	char *end = mangled + mangled_len;
+	DemString *ds = NULL;
+
+	for (size_t i = 0; i < mangled_len; ++i) {
+		char ch = tolower(mangled[i]);
+		if (!IS_LOWER(ch) && !IS_DIGIT(ch) && ch != '_' && ch != '$') {
+			goto demangle_fail;
+		}
+		mangled[i] = ch;
+	}
+
+	ds = dem_string_new();
+	if (!ds) {
+		goto demangle_fail;
+	}
+
+	if (next < end && (tmp = strstr(next, "$_$")) && tmp > next && IS_NAME(tmp[-1])) {
+		// <unit>$_$<object>_$_<unit1>_$$_<func_name>$<type0$type1>$$<ret_type>
+		dem_string_appends(ds, "unit ");
+		dem_string_append_n(ds, next, tmp - next);
+		dem_string_appends(ds, " ");
+		next = tmp + strlen("$_$");
+		while ((tmp = strstr(next, "_$_")) && tmp > next && IS_NAME(tmp[-1])) {
+			dem_string_append_n(ds, next, tmp - next);
+			dem_string_appends(ds, ".");
+			next = tmp + strlen("_$_");
+		}
+		if ((tmp = strstr(next, "_$$_")) && tmp == next) {
+			// often <unit1> is empty, thus we can skip it.
+			next += strlen("_$$_");
+		}
+	}
+
+	if (next < end && (tmp = strstr(next, "_$$_")) && tmp > next && IS_NAME(tmp[-1])) {
+		// <unit1>_$$_<func_name>$<type0$type1>$$<ret_type>
+		dem_string_appends(ds, "unit ");
+		dem_string_append_n(ds, next, tmp - next);
+		dem_string_appends(ds, " ");
+		next = tmp + strlen("_$$_");
+	}
+
+	if (next < end && (tmp = strchr(next, '$')) && tmp > next && IS_NAME(tmp[-1])) {
+		// <func_name>$<type0$type1>$$<ret_type>
+		dem_string_append_n(ds, next, tmp - next);
+		dem_string_appends(ds, "(");
+		next = tmp + strlen("$");
+		size_t n_arg = 0;
+
+		while (next < end && *next != '$' && (tmp = strchr(next, '$')) && tmp > next && IS_NAME(tmp[-1])) {
+			// <type0$type1>$$<ret_type>
+			if (n_arg < 1) {
+				dem_string_appendf(ds, "var a%u: ", n_arg);
+			} else {
+				dem_string_appendf(ds, "; a%u: ", n_arg);
+			}
+			dem_string_append_n(ds, next, tmp - next);
+			next = tmp + strlen("$");
+			n_arg++;
+		}
+
+		if (next < end && (tmp = strchr(next, '$'))) {
+			dem_string_appends(ds, ")");
+			// $$<ret_type>
+			next = tmp + strlen("$");
+			if (next < end) {
+				dem_string_appends(ds, " ");
+				dem_string_append(ds, next);
+			}
+		} else {
+			if (next < end) {
+				// <type0> (sometimes it may not have a return type just args.)
+				if (n_arg < 1) {
+					dem_string_appendf(ds, "var a%u: ", n_arg);
+				} else {
+					dem_string_appendf(ds, "; a%u: ", n_arg);
+				}
+				dem_string_append(ds, next);
+			}
+			dem_string_appends(ds, ")");
+		}
+	} else {
+		// <func_name>
+		dem_string_append(ds, next);
+		dem_string_appends(ds, "()");
+	}
+
+	if (ds->len < 1) {
+		goto demangle_fail;
+	}
+
+	free(mangled);
+	return dem_string_drain(ds);
+
+demangle_fail:
+	dem_string_free(ds);
+	free(mangled);
+	return NULL;
+}
+
+/**
+ * \brief Demangles pascal symbols
+ *
+ * Demangles pascal symbols
+ */
+char *libdemangle_handler_pascal(const char *mangled) {
+	if (!mangled || !strchr(mangled, '$')) {
+		return NULL;
+	}
+
+	size_t length = strlen(mangled);
+	if (length < 1) {
+		return NULL;
+	}
+
+	char *copy = strdup(mangled);
+	if (!copy) {
+		return NULL;
+	}
+
+	return demangle_free_pascal(copy, length);
+}
---
+++ test/test_pascal.c
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2022 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "demangling_unit.h"
+
+mu_demangle(0, pascal, "OUTPUT_$$_SQUARE$SMALLINT$$SMALLINT", "unit output square(var a0: smallint) smallint");
+mu_demangle(1, pascal, "OUTPUT_$$_init", "unit output init()");
+mu_demangle(2, pascal, "OUTPUT$_$MYOBJECT_$__$$_INIT$$QWORDBOOL", "unit output myobject.init() qwordbool");
+mu_demangle(3, pascal, "OUTPUT$_$MYOBJECT_$__$$_MYMETHOD", "unit output myobject.mymethod()");
+mu_demangle(4, pascal, "OUTPUT_$$_MYFUNC$$POINTER", "unit output myfunc() pointer");
+mu_demangle(5, pascal, "OUTPUT_$$_MYPROCEDURE$SMALLINT$LONGINT$PCHAR", "unit output myprocedure(var a0: smallint; a1: longint; a2: pchar)");
+mu_demangle(6, pascal, "OUTPUT_$$_MYFUNC2$SMALLINT$LONGINT$PCHAR$$POINTER", "unit output myfunc2(var a0: smallint; a1: longint; a2: pchar) pointer");
+mu_demangle(7, pascal, "OUTPUT_$$_MYFUN3$SMALLINT", "unit output myfun3(var a0: smallint)");
+mu_demangle(8, pascal, "OUTPUT_SQUARE$SMALLINT$$SMALLINT", "output_square(var a0: smallint) smallint");
+mu_demangle(9, pascal, "OUTPUT_INIT$$SMALLINT", "output_init() smallint");
+mu_demangle(10, pascal, "OUTPUT_INIT$SMALLINT", "output_init(var a0: smallint)");
+
+int all_tests() {
+	mu_demangle_run(0);
+	mu_demangle_run(1);
+	mu_demangle_run(2);
+	mu_demangle_run(3);
+	mu_demangle_run(4);
+	mu_demangle_run(5);
+	mu_demangle_run(6);
+	mu_demangle_run(7);
+	mu_demangle_run(8);
+	mu_demangle_run(9);
+	mu_demangle_run(10);
+	return tests_passed != tests_run;
+}
+
+mu_main(all_tests)
