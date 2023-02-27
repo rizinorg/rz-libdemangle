bc809f011e45e1c962a96b30dad2ceea41046f37
Dhruv Maroo <dhruvmaru007@gmail.com>
2023-02-27 10:16:25 +0530

    Implement demangler for Rust

--- src/rust.c
+++ src/rust.c
@@ -3,78 +3,77 @@
 #include "demangler_util.h"
 #include <rz_libdemangle.h>
 
-#define RS(from, to) \
-	if (replace_seq((const char **)&in, &out, (const char *)(from), to)) \
-	continue
+uint32_t get_integer(const char **str) {
+	/* Assuming the string in str is base 10 */
 
-static bool replace_seq(const char **in, char **out, const char *seq, char value) {
-	size_t len = strlen(seq);
+	uint32_t result = 0;
+	const char *x = *str;
 
-	if (strncmp(*in, seq, len)) {
-		return false;
+	while (IS_DIGIT(*x)) {
+		result *= 10;
+		result += *x & 0xf;
+		x++;
 	}
 
-	**out = value;
+	/* Update the string pointer */
+	*str = x;
 
-	*in += len;
-	*out += 1;
-
-	return true;
+	return result;
 }
 
+/**
+ * @brief We return NULL instead of strdup-ing the string, because that way we can check for NULL
+ * and invoke the CXX demangler \p sym again in case it a CXX symbol
+ * We should not call the CXX demangler here because then this code will not be LGPL,
+ * but GPL because CXX demangler is GPL
+ */
 char *libdemangle_handler_rust(const char *sym) {
-	int len;
-	char *str = NULL, *out, *in;
+	const char *post = sym;
+	char *prefixes[] = { "_ZN", /* Windows */ "ZN", /* OSX */ "__ZN" };
 
-	str = libdemangle_handler_cxx(sym);
-
-	if (!str) {
-		return str;
-	}
-
-	out = in = str;
-	len = strlen(str);
-
-	if (*in == '_') {
-		in++;
-		len--;
-	}
-
-	while ((len = strlen(in)) > 0) {
-		if (*in == '$') {
-			RS("$SP$", '@');
-			RS("$BP$", '*');
-			RS("$RF$", '&');
-			RS("$LT$", '<');
-			RS("$GT$", '>');
-			RS("$LP$", '(');
-			RS("$RP$", ')');
-			RS("$C$", ',');
-			// maybe a good idea to replace all utf-sequences by regexp \$u[0-9a-f]{2}\$ or so
-			RS("$u20$", ' ');
-			RS("$u22$", '\"');
-			RS("$u27$", '\'');
-			RS("$u2b$", '+');
-			RS("$u3b$", ';');
-			RS("$u5b$", '[');
-			RS("$u5d$", ']');
-			RS("$u7e$", '~');
-		}
-		if (*in == '.') {
-			if (len > 0 && in[1] == '.') {
-				in += 2;
-				*out++ = ':';
-				*out++ = ':';
-				len--;
-			} else {
-				in += 1;
-				*out = '-';
-			}
-		} else {
-			*out++ = *in++;
+	for (uint8_t i = 0; i < sizeof(prefixes) / sizeof(prefixes[0]); i++) {
+		uint8_t len = strlen(prefixes[i]);
+		if (!strncmp(sym, prefixes[i], len)) {
+			post += len;
+			break;
 		}
 	}
-	*out = '\0';
 
-	return str;
+	if (sym == post) {
+		/* Not a Rust symbol */
+		return NULL;
+	}
+
+	const char *itr = post;
+	while (*itr) {
+		/* Check if only ASCII chars present */
+		if (*itr & 0x80) {
+			return NULL;
+		}
+		itr++;
+	}
+
+	DemString *result = dem_string_new();
+
+	while (*post != 'E') {
+		uint32_t len = get_integer(&post);
+
+		/* Check if no digits found
+		OR If end of string reached
+		OR Element ends after (or when) the string ends */
+		if (len == 0 || *post == '\0' || len >= strlen(post)) {
+			/* All these cases are malformed */
+			dem_string_free(result);
+			return NULL;
+		}
+
+		dem_string_append_n(result, post, len);
+		post += len;
+
+		if (*post != 'E') {
+			dem_string_append(result, "::");
+		}
+	}
+
+	return dem_string_drain(result);
 }
