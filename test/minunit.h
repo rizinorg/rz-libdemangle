94429ea072b35226f7c1a3e0c8c329ce5ad339ad
wargio <wargio@libero.it>
2023-04-20 11:25:21 +0800

    Optimizing minunit to support only demangling

--- test/minunit.h
+++ test/minunit.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2015 Jeffrey Crowell <crowell@bu.edu>
 // SPDX-FileCopyrightText: 2018 ret2libc <sirmy15@gmail.com>
-// SPDX-FileCopyrightText: 2021 deroad <wargio@libero.it>
+// SPDX-FileCopyrightText: 2021-2023 deroad <wargio@libero.it>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 // minunit.h comes from http://www.jera.com/techinfo/jtns/jtn002.html
@@ -16,11 +16,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define RZ_API
+#include "rz_libdemangle.h"
+
+typedef struct mu_demangling_test_s {
+	int line;
+	const char *input;
+	const char *expected;
+} mu_demangling_test_t;
+
 typedef uint8_t ut8;
 typedef int bool;
-#define true 1
+#define true  1
 #define false 0
-
 
 #if __WINDOWS__
 #define TRED
@@ -50,196 +58,42 @@ typedef int bool;
 
 #define MU_BUF_SIZE 10240
 
-void snprint_mem(char *out, size_t out_size, const ut8 *buf, size_t len) {
-	size_t i;
-	*out = '\0';
-	for (i = 0; i < len; i++) {
-		size_t out_len;
-		if (i > 0) {
-			out_len = strlen(out);
-			snprintf(out + out_len, out_size - out_len, " ");
-		}
-		out_len = strlen(out);
-		snprintf(out + out_len, out_size - out_len, "%02x", buf[i]);
-	}
-}
-
-#define mu_assert(message, test) \
+#define mu_perror(line, message) \
 	do { \
-		if (!(test)) { \
-			mu_fail(message); \
-			mu_test_status = MU_TEST_UNBROKEN; \
-		} \
+		printf(TBOLD TRED "ERR\n[XX] Failed to demangle line %d: " TRESET "%s\n\n", line, message); \
 	} while (0)
 
-#define mu_perror(message) \
+#define mu_fail(line, message) \
 	do { \
-		if (mu_test_status != MU_TEST_BROKEN) { \
-			printf(TBOLD TRED "ERR\n[XX] Fail at line %d: " TRESET "%s\n\n", __LINE__, message); \
-		} else { \
-			printf(TBOLD TYELLOW "Broken at line %d: " TRESET "%s\n\n", __LINE__, message); \
-		} \
-	} while (0)
-
-#define mu_psyserror(message) \
-	do { \
-		perror(message); \
-		mu_perror(message); \
-	} while (0)
-
-#define mu_fail(message) \
-	do { \
-		mu_perror(message); \
+		mu_perror(line, message); \
 		if (mu_test_status != MU_TEST_BROKEN) \
 			return MU_ERR; \
 	} while (0)
 
-#define mu_ignore \
+#define mu_assert(line, message, test) \
 	do { \
-		printf(TYELLOW "IGN\n" TRESET); \
+		if (!(test)) { \
+			mu_fail(line, message); \
+			mu_test_status = MU_TEST_UNBROKEN; \
+		} \
+	} while (0)
+
+#define mu_end(line, input, expected) \
+	do { \
+		printf(TGREEN "OK" TRESET " line %d: '%s' -> '%s'\n", line, input, expected ? expected : "NULL"); \
 		return MU_PASSED; \
 	} while (0)
 
-#define mu_end \
-	do { \
-		printf(TGREEN "OK\n" TRESET); \
-		return MU_PASSED; \
-	} while (0)
-
-#define mu_cleanup_end \
-	do { \
-		if (retval == MU_PASSED) { \
-			mu_end; \
-		} else { \
-			return retval; \
-		} \
-	} while (0)
-
-#define mu_sysfail(message) \
-	do { \
-		perror(message); \
-		mu_fail(message); \
-	} while (0)
-
-#define mu_assert_true(actual, message) \
-	do { \
-		bool act__ = (actual); \
-		if (!(act__)) { \
-			char _meqstr[MU_BUF_SIZE]; \
-			snprintf(_meqstr, MU_BUF_SIZE, "%s: expected true, got false", (message)); \
-			mu_assert(_meqstr, false); \
-		} \
-	} while (0)
-
-#define mu_assert_false(actual, message) \
-	do { \
-		bool act__ = (actual); \
-		if ((act__)) { \
-			char _meqstr[MU_BUF_SIZE]; \
-			snprintf(_meqstr, MU_BUF_SIZE, "%s: expected false, got true", (message)); \
-			mu_assert(_meqstr, false); \
-		} \
-	} while (0)
-
-#define mu_assert_eq(actual, expected, message) \
-	do { \
-		ut64 act__ = (ut64)(actual); \
-		ut64 exp__ = (ut64)(expected); \
-		if ((exp__) != (act__)) { \
-			char _meqstr[MU_BUF_SIZE]; \
-			snprintf(_meqstr, MU_BUF_SIZE, "%s: expected %" PFMT64d ", got %" PFMT64d ".", (message), (ut64)(exp__), (ut64)(act__)); \
-			mu_assert(_meqstr, false); \
-		} \
-	} while (0)
-
-#define mu_assert_eqf(actual, expected, message) \
-	do { \
-		double act__ = (double)(actual); \
-		double exp__ = (double)(expected); \
-		if ((exp__) != (act__)) { \
-			char _meqstr[MU_BUF_SIZE]; \
-			snprintf(_meqstr, MU_BUF_SIZE, "%s: expected %lf, got %lf.", (message), exp__, act__); \
-			mu_assert(_meqstr, false); \
-		} \
-	} while (0)
-
-#define mu_assert_neq(actual, expected, message) \
+#define mu_assert_null(actual, input, line) \
 	do { \
 		char _meqstr[MU_BUF_SIZE]; \
-		ut64 act__ = (ut64)(actual); \
-		ut64 exp__ = (ut64)(expected); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected not %" PFMT64d ", got %" PFMT64d ".", (message), (exp__), (act__)); \
-		mu_assert(_meqstr, (exp__) != (act__)); \
+		void *act__ = (actual); \
+		snprintf(_meqstr, MU_BUF_SIZE, "expected %s to be NULL but instead got %s.", input, actual); \
+		free(act__); \
+		mu_assert(line, _meqstr, (act__) == NULL); \
 	} while (0)
 
-#define mu_assert_ptreq(actual, expected, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const void *act__ = (actual); \
-		const void *exp__ = (expected); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected %p, got %p.", (message), (exp__), (act__)); \
-		mu_assert(_meqstr, (exp__) == (act__)); \
-	} while (0)
-
-#define mu_assert_ptrneq(actual, expected, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const void *act__ = (actual); \
-		const void *exp__ = (expected); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected not %p, got %p.", (message), (exp__), (act__)); \
-		mu_assert(_meqstr, (exp__) != (act__)); \
-	} while (0)
-
-#define mu_assert_null(actual, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const void *act__ = (actual); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected to be NULL but it wasn't.", (message)); \
-		mu_assert(_meqstr, (act__) == NULL); \
-	} while (0)
-
-#define mu_assert_notnull(actual, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const void *act__ = (actual); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected to not be NULL but it was.", (message)); \
-		mu_assert(_meqstr, (act__) != NULL); \
-	} while (0)
-
-#define mu_assert_eq_fmt(actual, expected, message, fmt) \
-	do { \
-		ut64 act__ = (ut64)(actual); \
-		ut64 exp__ = (ut64)(expected); \
-		if ((exp__) != (act__)) { \
-			char _meqstr[MU_BUF_SIZE]; \
-			snprintf(_meqstr, MU_BUF_SIZE, "%s: expected " fmt ", got " fmt ".", (message), (exp__), (act__)); \
-			mu_assert(_meqstr, false); \
-		} \
-	} while (0)
-
-#define mu_assert_streq(actual, expected, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const char *act__ = (actual); \
-		act__ = act__ ? act__ : "(null)"; \
-		const char *exp__ = (expected); \
-		exp__ = exp__ ? exp__ : "(null)"; \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected %s, got %s.", (message), (exp__), (act__)); \
-		mu_assert(_meqstr, strcmp((exp__), (act__)) == 0); \
-	} while (0)
-
-#define mu_assert_strcontains(actual, expected, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const char *act__ = (actual); \
-		act__ = act__ ? act__ : "(null)"; \
-		const char *exp__ = (expected); \
-		exp__ = exp__ ? exp__ : "(null)"; \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected to find %s in %s.", (message), (exp__), (act__)); \
-		mu_assert(_meqstr, strstr((act__), (exp__)) != NULL); \
-	} while (0)
-
-#define mu_assert_streq_free(actual, expected, message) \
+#define mu_assert_streq_free(actual, expected, line) \
 	do { \
 		char *act2__ = (actual); \
 		char _meqstr[MU_BUF_SIZE]; \
@@ -247,36 +101,39 @@ void snprint_mem(char *out, size_t out_size, const ut8 *buf, size_t len) {
 		act__ = act__ ? act__ : "(null)"; \
 		const char *exp__ = (expected); \
 		exp__ = exp__ ? exp__ : "(null)"; \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected %s, got %s.", (message), (exp__), (act__)); \
+		snprintf(_meqstr, MU_BUF_SIZE, "expected %s, got %s.", (exp__), (act__)); \
 		int is_success = strcmp((exp__), (act__)) == 0; \
 		free(act2__); \
-		mu_assert(_meqstr, is_success); \
+		mu_assert(line, _meqstr, is_success); \
 	} while (0)
 
-#define mu_assert_nullable_streq(actual, expected, message) \
+#define mu_demangle_test_name(name) demangling_##name##_tests
+#define mu_demangle_func_name(name) demangle_with_##name
+#define mu_demangle_test_size(name) (sizeof(mu_demangle_test_name(name)) / sizeof((mu_demangle_test_name(name))[0]))
+
+#define mu_demangle_str_message(name, input, expected, line) \
 	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		const char *act__ = (actual); \
-		const char *exp__ = (expected); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected %s, got %s.", (message), (exp__ ? exp__ : "NULL"), (act__ ? act__ : "NULL")); \
-		mu_assert(_meqstr, ((act__) == NULL && (exp__) == NULL) || ((act__) != NULL && (exp__) != NULL && strcmp((exp__), (act__)) == 0)); \
+		char *output = libdemangle_handler_##name(input); \
+		if (expected) { \
+			mu_assert_streq_free(output, expected, line); \
+		} else { \
+			mu_assert_null(output, input, line); \
+		} \
 	} while (0)
 
-#define mu_assert_memeq(actual, expected, len, message) \
-	do { \
-		char _meqstr[MU_BUF_SIZE]; \
-		size_t _meqstr_len; \
-		const ut8 *act__ = (actual); \
-		const ut8 *exp__ = (expected); \
-		snprintf(_meqstr, MU_BUF_SIZE, "%s: expected ", message); \
-		_meqstr_len = strlen(_meqstr); \
-		snprint_mem(_meqstr + _meqstr_len, MU_BUF_SIZE - _meqstr_len, (exp__), (len)); \
-		_meqstr_len = strlen(_meqstr); \
-		snprintf(_meqstr + _meqstr_len, MU_BUF_SIZE - _meqstr_len, ", got "); \
-		_meqstr_len = strlen(_meqstr); \
-		snprint_mem(_meqstr + _meqstr_len, MU_BUF_SIZE - _meqstr_len, (act__), (len)); \
-		mu_assert(_meqstr, memcmp((exp__), (act__), (len)) == 0); \
-	} while (0)
+#define mu_demangle_with(name) \
+	bool mu_demangle_func_name(name)(mu_demangling_test_t * test) { \
+		mu_demangle_str_message(name, test->input, test->expected, test->line); \
+		mu_end(test->line, test->input, test->expected); \
+	}
+
+#define mu_demangle_tests(name, ...) \
+	mu_demangling_test_t mu_demangle_test_name(name)[] = { \
+		__VA_ARGS__ \
+	}
+
+#define mu_demangle_test(input, expected) \
+	{ __LINE__, input, expected }
 
 #define mu_run_test_named(test, name, ...) \
 	do { \
@@ -288,27 +145,30 @@ void snprint_mem(char *out, size_t out_size, const ut8 *buf, size_t len) {
 		tests_passed += result; \
 	} while (0)
 
-#define mu_run_test(test, ...) mu_run_test_named(test, #test, __VA_ARGS__)
+#define mu_demangle_loop(name, demangler) \
+	do { \
+		for (size_t i = 0; i < mu_demangle_test_size(name); ++i) { \
+			mu_demangling_test_t *t = &mu_demangle_test_name(name)[i]; \
+			mu_run_test_named(mu_demangle_func_name(demangler), #name, t); \
+		} \
+	} while (0)
 
-#define mu_cleanup_fail(label, message) \
-	do { \
-		mu_perror(message); \
-		retval = MU_ERR; \
-		goto label; \
-	} while (0)
-#define mu_cleanup_sysfail(label, message) \
-	do { \
-		mu_psyserror(message); \
-		retval = MU_ERR; \
-		goto label; \
-	} while (0)
 int tests_run = 0;
 int tests_passed = 0;
 int mu_test_status = MU_TEST_UNBROKEN;
 
-#define mu_main(fcn) \
+#define mu_main(name, demangler) \
+	mu_demangle_with(demangler); \
 	int main(int argc, char **argv) { \
-		return fcn(); \
+		mu_demangle_loop(name, demangler); \
+		return tests_passed != tests_run; \
+	}
+
+#define mu_main2(name) \
+	mu_demangle_with(name); \
+	int main(int argc, char **argv) { \
+		mu_demangle_loop(name, name); \
+		return tests_passed != tests_run; \
 	}
 
 #endif
