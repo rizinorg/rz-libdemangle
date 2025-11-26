e2852579db21996f2cef8bda00d767725026ae20
billow <billow.fun@gmail.com>
2025-11-26 14:52:14 +0800

    test: add unit tests for demangle rule prefix functionality

---
+++ test/unit/test_cxx_rules.c
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2025 billow <billow.fun@gmail.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#include "rz-minunit.h"
+#include "../../src/cplusplus/v3.h"
+
+int test_rule_prefix() {
+	CpDemOptions options = {};
+	mu_assert_streq_free(demangle_rule("DC1a2abE", rule_prefix, options), "aab", "");
+	mu_end;
+}
+
+int all_tests() {
+	mu_run_test(test_rule_prefix);
+	return tests_passed != tests_run;
+}
+
+mu_main(all_tests);
