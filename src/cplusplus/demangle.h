406a4e6b5b6a2fea72abd0d002c835d1b69c1863
billow <billow.fun@gmail.com>
2026-02-04 23:14:20 +0800

    Refactor demangle options conversion and improve consistency in demangling functions

--- src/cplusplus/demangle.h
+++ src/cplusplus/demangle.h
@@ -5,6 +5,8 @@
 #ifndef CP_DEMANGLE_H
 #define CP_DEMANGLE_H
 
+#include "rz_libdemangle.h"
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -15,6 +17,23 @@ typedef enum {
 	DEM_OPT_SIMPLE = 1 << 2, /**< \b Simplify the output, to make it more human readable */
 	DEM_OPT_ALL = 0xff /**< \b Everything, everywhere, all at once! */
 } CpDemOptions;
+
+static inline CpDemOptions cp_options_convert(RzDemangleOpts opts) {
+	CpDemOptions copts = DEM_OPT_ANSI | DEM_OPT_PARAMS;
+	if (opts & RZ_DEMANGLE_OPT_ENABLE_ALL) {
+		copts |= DEM_OPT_ALL;
+	}
+	if (opts & RZ_DEMANGLE_OPT_SIMPLIFY) {
+		copts |= DEM_OPT_SIMPLE;
+	}
+	if (opts & RZ_DEMANGLE_OPT_BASE) {
+		// no corresponding flag in CpDemOptions
+	}
+	if (opts == 0) {
+		copts = DEM_OPT_ALL - DEM_OPT_SIMPLE;
+	}
+	return copts;
+}
 
 char *cp_demangle_v2(const char *mangled, CpDemOptions opts);
 char *cp_demangle_v3(const char *mangled, CpDemOptions opts);
